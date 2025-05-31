ca594b3fc5d1274733c5fede7ad1ab69844c7463
motl19 <olaegbeadesola@gmail.com>
2025-05-31 23:53:02 +0400

    db-model_config

---
+++ models/user.models.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Username is required!'],
+        trim: true,
+        minLength: 2,
+        maxLength: 50
+    },
+
+    email: {
+        type: String,
+        required: [true, 'User Email is required!'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'User Password is required!'],
+        minLength: 6,
+        maxLength: 100
+    },
+},
+    { timestamps: true, });
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
+// { name: 'John Doe', email: 'j.doe@gmail.com', password: 'password123' }
