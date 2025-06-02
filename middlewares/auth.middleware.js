3b2b800bc59e02e8f450c71f3f7b845a0a8818ef
motl19 <olaegbeadesola@gmail.com>
2025-06-02 23:00:05 +0400

    user_endpoints and authorization-arcjet

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+import User from '../models/user.models.js';
+
+
+const authorize = async (req, res, next) => {
+    try {
+
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) return res.status(401).json({ message: 'Unauthorized, no token provided!' });
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId);
+
+        if (!user) return res.status(401).json({ message: 'Unauthorized, user not found!' });
+        req.user = user;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
+}
+
+export default authorize;
